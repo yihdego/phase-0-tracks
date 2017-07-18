643b466330da309fa5487b41991ad002c85331ff
Amanuel Yihdego <yihdego@msn.com>
2017-07-18 13:35:53 -0700

    testing age program

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,8 +1,15 @@
-	puts "what is your name?"
-	resp_name = gets.chomp
-	puts "how old are you?"
-	age = gets.chomp
-	puts "can we order you garlic?"
-	resp_garlic = gets.chomp
-	puts "Would you like to endroll in company healthcare?"
-	resp_health = gets.chomp
+
+puts "what is your name?"
+resp_name = gets.chomp
+puts "how old are you?"
+age = gets.chomp
+puts "can we order you garlic?"
+resp_garlic = gets.chomp
+puts "Would you like to endroll in company healthcare?"
+resp_health = gets.chomp
+
+if age >= 100
+	puts "Probably a vampire"
+else
+	puts "Probably a werewolf"
+end
