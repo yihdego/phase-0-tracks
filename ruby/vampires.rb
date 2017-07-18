97c2b3ea9ddc1ae9f059b4627f08a2b23c6a296f
Amanuel Yihdego <yihdego@msn.com>
2017-07-18 13:55:44 -0700

    figured out how to get string into integer

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,15 +1,17 @@
 
-puts "what is your name?"
+puts "What is your name?"
 resp_name = gets.chomp
-puts "how old are you?"
-age = gets.chomp
-puts "can we order you garlic?"
+puts "How old are you?"
+age = gets.chomp.to_i
+puts " What year were you born?"
+year_born = gets.chomp.to_i
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 resp_garlic = gets.chomp
-puts "Would you like to endroll in company healthcare?"
+puts "Would you like to enroll in the company’s health insurance?"
 resp_health = gets.chomp
 
-if age >= 100
-	puts "Probably a vampire"
-else
+if year_born = 2017 - age
 	puts "Probably a werewolf"
+else
+	puts "Probably a vampire"
 end
