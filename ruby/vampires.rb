df52e614691cde9ad34351eccb5f7839b311f5bf
Amanuel Yihdego <yihdego@msn.com>
2017-07-18 12:05:31 -0700

    made vampire questionarre

---
+++ ruby/vampires.rb
@@ -0,0 +1,8 @@
+	puts "what is your name?"
+	resp_name = gets.chomp
+	puts "how old are you?"
+	age = gets.chomp
+	puts "can we order you garlic?"
+	resp_garlic = gets.chomp
+	puts "Would you like to endroll in company healthcare?"
+	resp_health = gets.chomp
