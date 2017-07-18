e91aa9da9db44009667a50d22de5e6ae1c4bc7d2
Amanuel Yihdego <yihdego@msn.com>
2017-07-18 14:24:02 -0700

    changed variables from strings and intergers to booleans

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,16 +2,23 @@
 puts "What is your name?"
 resp_name = gets.chomp
 puts "How old are you?"
-age = gets.chomp.to_i
-puts " What year were you born?"
-year_born = gets.chomp.to_i
+age = gets.to_i
+puts "What year were you born?"
+year_born = gets.to_i
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 resp_garlic = gets.chomp
 puts "Would you like to enroll in the company’s health insurance?"
 resp_health = gets.chomp
 
-if year_born = 2017 - age
-	puts "Probably a werewolf"
+
+if year_born == (2017-age)
+	lied_age = false
 else
-	puts "Probably a vampire"
+	lied_age = true
+end
+
+if resp_health == "yes"
+	immortality = true
+else
+	immortality = false
 end
