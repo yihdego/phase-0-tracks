6431e32448cc56ae2871e00ebf523dc3db9da818
Amanuel Yihdego <yihdego@msn.com>
2017-07-19 00:35:25 -0700

    debugging, cant get almost and certainly vampire

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -25,9 +25,9 @@ while processed_applicants < goal
 	end
 
 	if resp_health == "yes" || resp_health == "Yes"
-		immortality = true
+		immortality = false
 	else resp_health == "no" || resp_health == "No"
-		immortality == false
+		immortality = true
 	end
 
 	if resp_garlic == "yes" || resp_garlic == "Yes"
@@ -36,16 +36,18 @@ while processed_applicants < goal
 		allergic = true
 	end
 
-	if lied_age == false && allergic == false || immortality == false
+case
+	when lied_age == false && allergic == false || immortality == false
 		puts "Probably not a vampire."
-	elsif lied_age == true && allergic == false || immortality == true
-		puts " Probably a vampire."
-	elsif lied_age == true && allergic == true && immortality == true
+	when lied_age == true && (allergic == true || immortality == false) || (allergic == false || immortality == true)
+		puts "Probably a vampire."
+	when lied_age == true && (allergic == true && immortality == true)
 		puts "Almost certainly a vampire."
-	elsif resp_name == "Drake Cula"
+	when resp_name == "Drake Cula"
 		puts "Definitely a vampire."
-	else
+	when
 		puts "Results inconclusive."
 	end
+end	
 	processed_applicants +=1
 end
