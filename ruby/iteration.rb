706afd5e711311d59e018d78b8b96fa56a5d7dfd
Amanuel Yihdego <yihdego@msn.com>
2017-07-25 12:59:10 -0700

    almost finished

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -27,3 +27,30 @@ capital_cities.each do |key, value|
 end
 
 puts capital_cities
+
+
+states_provinces = [50, 23, 26]
+states_provinces.delete_if {|number| number < 25}
+puts states_provinces
+
+lunches = {"Taco" => 2, "Burrito" => 6, "Hot Dog" => 1}
+lunches.delete_if {|key, value| value >= 5}
+puts lunches
+
+states_provinces = [50, 23, 26]
+states_provinces.keep_if {|number| number == 50}
+puts states_provinces
+
+lunches = {"Taco" => 2, "Burrito" => 6, "Hot Dog" => 1}
+lunches.keep_if {|key, value| value == 1}
+puts lunches
+
+grades = [104, 3, 0, 98, 27].select { |grades| grades.even?}
+p grades
+
+tv_res = {"Sony" => 4000, "Toshiba" =>1920, "Panasonic" => 800}
+puts tv_res.has_value?(4000)
+puts tv_res.has_key?("Sanyo")
+
+a = [1,2,3,4]
+a.select {|n|n >2}
