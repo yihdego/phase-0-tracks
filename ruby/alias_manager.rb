431e03e33fa2b8c48aff6e58d8fc584f5f72785b
Amanuel Yihdego <yihdego@msn.com>
2017-07-29 08:59:10 -0700

    psuedocoding

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,10 @@
+# Inputing the first name variable then the last name variable.
+# making them a variable where the last is printed before the first
+# identifying all [a,e,i,o,u] and comparing them to another variable string "aeiou" and replacing them with a different index
+
+puts "what is your name secret agent?"
+name = gets.chomp
+alias_name = name.split (' ')
+puts [alias_name[1],alias_name[0]]
+secret_name = [alias_name[1]+" "+alias_name[0]]
+puts secret_name
