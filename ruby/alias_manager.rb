65eda08153c1695c121037729aabd9a76f6123cb
Amanuel Yihdego <yihdego@msn.com>
2017-07-30 02:31:20 -0700

    adding loop

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -9,7 +9,25 @@ puts [alias_name[1],alias_name[0]]
 secret_name = [alias_name[1]+" "+alias_name[0]]
 puts secret_name
 
-letters = alias_name[1].split('')
-letter
- alias_name[0].split('')
+vowel = ['a','e','i','o','u']
 
+agent_name = alias_name[1].split('')
+p agent_name.at(0)
+p agent_name.select {|vowel| vowel}
+p agent_name.select { |v| v=~/[aeiou]/ }
+p agent_name.join
+
+
+index = 0
+while index < alias_name.length
+	v=['a','e','i','o','u']
+	l=secret_name[index]
+	l_next=(v.index(l).rotate)
+	secret_name[index]=v[l_next]
+	index += 1
+end
+puts secret_name
+
+# if agent_name.select { |v| v=~/[aeiou]/ }
+# 	agent_name.at(index+1)
+# end
