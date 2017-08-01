efcd9b4f17d3bcce4b7e38282860af8f7fed01a6
Amanuel Yihdego <yihdego@msn.com>
2017-08-01 15:45:46 -0700

    release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -7,3 +7,34 @@ class Puppy
 
 end
 
+buddy = Puppy.new
+
+p buddy.fetch("ball")
+
+def speak(repeat)
+	index = 0
+	while index < repeat
+		puts "woof"
+		index += 1
+	end
+end
+
+speak(2)
+
+def roll_over
+	puts "rolls over"
+end
+
+roll_over
+
+def dog_years(human_years)
+	(human_years)*7
+end
+
+dog_years(5)
+
+def play_dead
+	puts "lies down and rolls on back"
+end
+
+play_dead
