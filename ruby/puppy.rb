7f788425df8f6c880c43d0b3789004c14a3c821c
Amanuel Yihdego <yihdego@msn.com>
2017-08-01 00:33:55 -0700

    release 0

---
+++ ruby/puppy.rb
@@ -0,0 +1,13 @@
+# Species
+# Puppysaurus
+
+# Characteristics
+# 4 legs: yes
+# slobbering tongue: yes
+# fur color: varies
+# weight: varies
+# size of bark: varies
+
+# Behavior
+# Cute at everything
+# trouble getting tendencies
