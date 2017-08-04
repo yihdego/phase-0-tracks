cf6a5bf603dc2cd9989409289c9e1126756ad16a
Amanuel Yihdego <yihdego@msn.com>
2017-08-04 16:33:07 -0700

    added getter and setter methods for different attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,15 +1,39 @@
 class Santa
+	def initialize(gender,ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity
+		@age = 0
+		@reindeer_ranking= 	["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	end
+	def gender= (assigned_gender)
+	  @gender = assigned_gender
+	end
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}"
 	end
-	def initialize
-		puts "Initializing Santa instace..."
+	def celebrate_birthday=(new_birthday)
+		@age = new_birthday
+	end
+	def get_mad_at= (which_reindeer)
+ @reindeer_ranking.delete(which_reindeer)
+ @reindeer_ranking << (which_reindeer)
+	end
+	def age
+	  @age
+	end
+	def ethnicity
+	  @ethnicity
 	end
 end
 
-santa = Santa.new
-santa.speak
-santa.eat_milk_and_cookies("oreo")
+
+santa = Santa.new("Female", "Hispanic")
+santa.celebrate_birthday = 1
+santa.get_mad_at = "Vixen"
+p santa
+santa.gender = "undisclosed"
+p santa.age
+p santa.ethnicity
