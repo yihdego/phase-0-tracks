5ffab3051b54569042fc346c3adb3379c1ec8cd5
Amanuel Yihdego <yihdego@msn.com>
2017-08-03 22:39:18 -0700

    made mixin module for shout

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,34 @@
+# module Shout
+#   def self.yell_angrily(words)
+#   	words + "!!!" + ":("
+#   end
+#   def self.yelling_happily(words)
+#   	words + "!!!" + "=3"
+#   end
+# end
+
+# p Shout.yell_angrily("Hello")
+# p Shout.yelling_happily("Hello")
+
 module Shout
-  def self.yell_angrily(words)
-  	words + "!!!" + ":("
-  end
-  def self.yelling_happily(words)
-  	words + "!!!" + "=3"
-  end
+	def yelling_angrily(words)
+   	puts words + "!!!" + ":("
+    end
+	def yelling_happily(words)
+   puts words + "!!!" + "=3"
+    end
 end
 
-p Shout.yell_angrily("Hello")
-p Shout.yelling_happily("Hello")
+class Seahawks
+	include Shout
+end
+
+class Wrestling_fan
+	include Shout
+end
+
+seahawks = Seahawks.new
+seahawks.yelling_happily("Go Hawks ")
+
+mark = Wrestling_fan.new
+mark.yelling_angrily("Boo ")
