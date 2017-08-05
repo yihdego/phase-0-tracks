dcd4be4fbce4f6ef9b26e476f9408f01e6a0a990
Amanuel Yihdego <yihdego@msn.com>
2017-08-04 18:33:04 -0700

    passed all 4 tests

--- ruby/list/TodoList.rb
+++ ruby/list/TodoList.rb
@@ -1,9 +1,27 @@
 class TodoList
-	def get_items(x,y)
-	todolist = [x,y]
+	def initialize(list)
+		@list = list
 	end
-	
+	def get_items
+		@list
+	end
+	def add_item(z)
+	@list = @list << z
+	end
+	def delete_item(w)
+	@list.delete(w)
+	end
+	def get_item(index)
+		@list.at(index)
+	end
 end
+
+# class TodoList
+# 	def get_items(x,y)
+# 	todolist = [x,y]
+# 	end
+	
+# end
 
 # def get_items(x,y)
 # 	[x,y]
