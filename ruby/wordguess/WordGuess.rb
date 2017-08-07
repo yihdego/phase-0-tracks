a1215cd241f7405aa26f7cadf9aadd7c0258a36f
Amanuel Yihdego <yihdego@msn.com>
2017-08-07 08:43:18 -0700

    added and deleting comments in code

--- ruby/wordguess/WordGuess.rb
+++ ruby/wordguess/WordGuess.rb
@@ -54,8 +54,9 @@ class GuessWord
       end
     end
   end
-  
 end
+
+# User Input
 puts "Player 1 give me a word"
 player1word = gets.chomp
 game = GuessWord.new(player1word)
@@ -64,36 +65,3 @@ game.puzzle = game.blanks.split(' ')
 puts "Player 2 guess #{game.guesses} times what letters are in this patter (#{game.blanks})"
 guess = gets.chomp
 game.letter_locator(guess)
-
-
-class GuessWord
-  def initialize(input)
-    puts "initialize game..."
-    @word = input
-    @blanks = []
-  end
-  def word
-    @word
-  end 
-  def blanks
-    @blanks
-  end
-  def word=(new_word)
-    @word = new_word
-  end
-  def blanks=(word)
-    how_blanks = @word.length
-    index = 0
-    while index < how_blanks
-      @blanks.push('_ ')
-      index += 1
-    end
-    @blanks = blanks.join
-  end
-  
-end
-
-# game = GuessWord.new("yellow")
-# p game.word
-# game.blanks = game.word
-# p game.blanks
