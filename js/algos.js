22e810d07dca7b239ca32e35f81aeca8925f1e5f
Amanuel Yihdego <yihdego@msn.com>
2017-08-11 21:14:39 -0700

    made function that makes random strings based on length

--- js/algos.js
+++ js/algos.js
@@ -37,17 +37,37 @@ if ((object_1[0] == object_2[0])||(object_1[1] == object_2[1])){
 }
 }
 
+
 // Write a function that takes an interger for the length and builds and returns an array of strings
 // of the given lengths. So if we ran your function with an argument of 3
 // we would get an array of 3random words back. Words should be randomly varying length, 
 // with a minimum of 1 letter or maximum of 10. random generator 
 
-function random10{
-return Math.floor(Math.random()*10)+1;
-};
+//psuedocode: function takes 1 variable of numbers. input an interger as a variable. make a variable in function of the alphabet.
+// make a loop of the variable of the number. make a variable of an array that is the lenght of the initial input.
+// in each part of the loop the loop there is a new variable of string with randomly determinded lenght of 1-10.
+// random string is maybe initially initialized with ' ' and then is replaced with random characters in a variable of the alphabet.
+// end loop portion of the loop by adding stringinto empty array until array is finished then return or print said array.
+// new solution, make a loop where random letters are generated and inserted into a word string using word = word + letter
 
+function randomword(){
+	var word = '';
+	for (var i = 0; i < (Math.floor(Math.random()*10)+1); i++){
+	var alphabet = ('abcdefghijklmnopqrstuvwxyz'[Math.floor(Math.random()*25)]);
+	word += alphabet;
+}
+return word;
+}
+function array(x){
+	var array = [];
+	for (var i = 0; i < x ; i ++){
+	array += randomword();
+}
+return array;
+}
 
 // Driver Code
 console.log(longest(["long phrase", "longest phrase", "longer phrase"]))
 are_they_same(Steven, Tamir);
-console.log(function random10);
+console.log(randomword());
+console.log(array(3));
