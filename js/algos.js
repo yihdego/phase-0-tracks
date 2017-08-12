96527a2ecab7c9d3c75d2e5341c91ddaecce92da
Amanuel Yihdego <yihdego@msn.com>
2017-08-11 20:07:20 -0700

    made psuedocode for random word array

--- js/algos.js
+++ js/algos.js
@@ -16,7 +16,7 @@ for (var i = 0; i < array.length; i++){
 	}
 }	return long;
 }
-console.log(longest(["long phrase", "longest phrase", "longer phrase"]))
+
 
 // write a function that takes two objects and checks to see if the objects share at least one key-value pair.
 
@@ -25,18 +25,29 @@ console.log(longest(["long phrase", "longest phrase", "longer phrase"]))
 
 // psuedocode: takes two the object value's and store into 2 variables
 // there should be an if/else statement, if the 2 variables at value are equal then puts true else statement is false
-
-var object_1 = {name: "Steven", age: 54}
-var object_2= {name: "Tamir", age: 54}
+var Steven = {name: "Steven", age: 54};
+var Tamir = {name: "Tamir", age: 54};
 
 function are_they_same(object_1, object_2) {
 
-	var first_item = object_1
-	var second_item = object_2
-if {first_item[0] == second_item[0] || first_item[1] == second_item[1]
-console.log("true")
+if ((object_1[0] == object_2[0])||(object_1[1] == object_2[1])){
+  return console.log("true");
 }else{
-console.log("false")	
+  return console.log("false");	
+}
 }
 
+// Write a function that takes an interger for the length and builds and returns an array of strings
+// of the given lengths. So if we ran your function with an argument of 3
+// we would get an array of 3random words back. Words should be randomly varying length, 
+// with a minimum of 1 letter or maximum of 10. random generator 
 
+function random10{
+return Math.floor(Math.random()*10)+1;
+};
+
+
+// Driver Code
+console.log(longest(["long phrase", "longest phrase", "longer phrase"]))
+are_they_same(Steven, Tamir);
+console.log(function random10);
