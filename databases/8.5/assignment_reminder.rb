42096888a334ae06a9613e2093167f4a9b61efcd
Amanuel Yihdego <yihdego@msn.com>
2017-08-24 11:58:41 -0700

    made method for retrieving due dates

--- databases/8.5/assignment_reminder.rb
+++ databases/8.5/assignment_reminder.rb
@@ -1,6 +1,7 @@
 require 'sqlite3'
 
-homework = SQLite3::Database.new("assignments.db")
+@homework = SQLite3::Database.new("assignments.db")
+@homework.results_as_hash = true
 
 create_homework_log = <<-SQL
   CREATE TABLE IF NOT EXISTS assignments(
@@ -10,27 +11,32 @@ create_homework_log = <<-SQL
   )
 SQL
 
-homework.execute(create_homework_log)
-
-response = nil
-until response = "dont"
-	puts "Do you want to know what homework is due or do you want to add a new asisngment? Respond with due and add respectively. Or done when you want to exit program."
-	response = gets.chomp
-	if response = "due"
-		assignments_due
-	elsif response = "add"
-		assignments_add
-	else
-		puts "That wasn't clear"
-	end
-end
+@homework.execute(create_homework_log)
 
 def assignments_add
-	homework.execute("INSERT INTO assignments (assignment, due) VALUES (?, ?)", [assingment, due])
+	puts "What homework is due"
+	homework_add = gets.chomp
+	puts "When is that assignment due"
+	due_date = gets.chomp
+	@homework.execute("INSERT INTO assignments (assignment, due) VALUES (?, ?)", [homework_add, due_date])
 end
 
 def assignments_due
-	assignments.each_pair do |assignment, due|
-		puts "The #{assignment} is due on #{due}."
+	assignment_reminder = @homework.execute("SELECT * FROM assignments")
+	assignment_reminder.each do |assignment|
+		puts "The #{assignment['assignment']} is due on #{assignment['due']}."
+	end
+end
+
+response = nil
+until response == "done"
+	puts "Do you want to know what homework is due or do you want to add a new asisngment? Respond with due and add respectively. Or done when you want to exit program."
+	response = gets.chomp
+	if response == "due"
+		assignments_due
+	elsif response == "add"
+		assignments_add
+	else
+		puts "Ok goodbye"
 	end
 end
