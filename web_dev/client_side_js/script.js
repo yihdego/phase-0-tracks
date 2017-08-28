bcb7eb0a4b1eefa64a48ebc1a5e23be784e90220
Amanuel Yihdego <yihdego@msn.com>
2017-08-27 20:31:34 -0700

    refactored

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -2,10 +2,8 @@ console.log("hello world");
 
 
 
-function changefontitalic(){
-	var testing = document.getElementsByTagName('p');
-	var testingtext = testing[0];
-	testingtext.style.fontStyle = "italic";	
+function changefontitalic(event){
+	event.target.style.fontStyle = "italic";	
 }
 
 var testing = document.getElementsByTagName('p');
